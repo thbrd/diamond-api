d68374ca4cd535dbd4f301e7f81fb563ba4950a0
thbrd <lars.fikenscher@gmail.com>
2025-07-20 21:43:52 +0200

    Add files via upload

--- paintbynumbersgenerator.py
+++ paintbynumbersgenerator.py
@@ -9,10 +9,17 @@ def generate_paint_by_numbers(image: Image.Image, num_colors: int = 24) -> Image
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
     # Rescale image for clustering
-    max_size = 800
+    max_size = 1024  # Langste zijde max, behoud aspect ratio
     if max(img.shape[:2]) > max_size:
         scale = max_size / max(img.shape[:2])
-        img = cv2.resize(img, (int(img.shape[1] * scale), int(img.shape[0] * scale)), interpolation=cv2.INTER_AREA)
+        
+    h, w = img.shape[:2]
+    if max(h, w) > max_size:
+        scale = max_size / max(h, w)
+        new_w = int(w * scale)
+        new_h = int(h * scale)
+        img = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_NEAREST)
+
 
     Z = img.reshape((-1, 3))
     Z = np.float32(Z)
