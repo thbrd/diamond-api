9e04ab849ec2dc523af6853aad73eaacf2dee101
thbrd <lars.fikenscher@gmail.com>
2025-07-20 22:20:16 +0200

    Add files via upload

--- paintbynumbersgenerator.py
+++ paintbynumbersgenerator.py
@@ -36,10 +36,9 @@ def generate_paint_by_numbers(image: Image.Image, num_colors: int = 24) -> Image
     label_image = labels.reshape((img.shape[0], img.shape[1]))
     contour_img = clustered_img.copy()
     font = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = 1.2
-    line_thickness = 3
-
-    for label_val in range(num_colors):
+    scale_multiplier = 3
+    font_scale = 0.4 * scale_multiplier
+    line_thickness = int(1 * scale_multiplier)for label_val in range(num_colors):
         mask = np.uint8(label_image == label_val)
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -48,14 +47,14 @@ def generate_paint_by_numbers(image: Image.Image, num_colors: int = 24) -> Image
                 continue
 
             # Draw contour
-            cv2.drawContours(contour_img, [cnt], -1, (0, 0, 0), line_thickness)
+            cv2.drawContours(contour_img, [cnt], -1, (0, 0, 0), line_thickness, cv2.LINE_AA)
 
             # Get center of contour and label
             M = cv2.moments(cnt)
             if M["m00"] != 0:
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
-                cv2.putText(contour_img, str(label_val + 1), (cX - 5, cY + 5), font, font_scale, (0, 0, 0), line_thickness, cv2.LINE_AA)
+                cv2.putText(contour_img, str(label_val + 1), (cX - 5 * scale_multiplier, cY + 5 * scale_multiplier), font, font_scale, (0, 0, 0), line_thickness, cv2.LINE_AA)
 
     contour_img = cv2.cvtColor(contour_img, cv2.COLOR_BGR2RGB)
     # === Vergelijkbaar met svgSizeMultiplier: verhoog resolutie van eindresultaat ===
