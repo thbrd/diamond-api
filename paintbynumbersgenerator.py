204f6b4db942fbfb98afcc918ef25218e9f03b79
thbrd <lars.fikenscher@gmail.com>
2025-07-21 09:59:21 +0200

    Add files via upload

--- paintbynumbersgenerator.py
+++ paintbynumbersgenerator.py
@@ -11,37 +11,46 @@ def generate_paint_by_numbers(image, num_colors, static_folder="static"):
     if str(num_colors) not in COLOR_PALETTES:
         raise ValueError(f"Unsupported number of colors: {num_colors}")
 
+    original_size = image.size
+    scaled_img = image.copy()
+    MAX_ANALYZE_SIZE = 400
+    scaled_img.thumbnail((MAX_ANALYZE_SIZE, MAX_ANALYZE_SIZE), Image.Resampling.BICUBIC)
+
     palette = np.array(COLOR_PALETTES[str(num_colors)])
-    img = image.convert("RGB")
-    width, height = img.size
-    img_np = np.array(img).reshape(-1, 3)
+    img_np = np.array(scaled_img).reshape(-1, 3)
 
     diffs = np.linalg.norm(img_np[:, None, :] - palette[None, :, :], axis=2)
     closest = np.argmin(diffs, axis=1)
-    labels = closest.reshape((height, width))
+    labels = closest.reshape((scaled_img.height, scaled_img.width))
     remapped = palette[labels]
 
     color_img = Image.fromarray(remapped.astype(np.uint8))
-    number_img = Image.new("RGB", (width, height), "white")
-    draw = ImageDraw.Draw(number_img)
+    upscaled_color = color_img.resize(original_size, Image.NEAREST)
 
+    number_img = Image.new("RGB", scaled_img.size, "white")
+    draw = ImageDraw.Draw(number_img)
     try:
         font = ImageFont.load_default()
     except Exception:
         font = None
 
-    for y in range(0, height, 10):
-        for x in range(0, width, 10):
+    for y in range(0, scaled_img.height, 10):
+        for x in range(0, scaled_img.width, 10):
             label = str(int(labels[y][x]) + 1)
             if font:
                 draw.text((x, y), label, fill="black", font=font)
 
-    # SVG-bestand genereren
-    svg = f"<svg xmlns='http://www.w3.org/2000/svg' width='{width}' height='{height}'>"
-    for y in range(0, height, 10):
-        for x in range(0, width, 10):
+    # SVG genereren
+    svg = f"<svg xmlns='http://www.w3.org/2000/svg' width='{original_size[0]}' height='{original_size[1]}'>"
+    scale_x = original_size[0] / scaled_img.width
+    scale_y = original_size[1] / scaled_img.height
+
+    for y in range(0, scaled_img.height, 10):
+        for x in range(0, scaled_img.width, 10):
             label = str(int(labels[y][x]) + 1)
-            svg += f"<text x='{x}' y='{y + 8}' font-size='10' fill='black'>{label}</text>"
+            sx = int(x * scale_x)
+            sy = int(y * scale_y)
+            svg += f"<text x='{sx}' y='{sy + 8}' font-size='10' fill='black'>{label}</text>"
     svg += "</svg>"
     svg = svg.encode("utf-8").decode("utf-8")
 
@@ -53,5 +62,5 @@ def generate_paint_by_numbers(image, num_colors, static_folder="static"):
     with open(svg_path, "w", encoding="utf-8") as f:
         f.write(svg)
 
-    color_img.save(png_path, format="PNG")
+    upscaled_color.save(png_path, format="PNG")
     return uid
